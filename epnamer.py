bafe5e575da10ea4f9272a268c79fd236054f826
Louis Warren <louis@lsw.nz>
2016-03-28 20:57:40 +1300

    Get episode data

--- epnamer.py
+++ epnamer.py
@@ -6,6 +6,7 @@ import urllib.request
 target_dir = "~/epnamer/test"
 
 show_search_url = "http://api.tvmaze.com/singlesearch/shows?q={}"
+episode_search_url = "http://api.tvmaze.com/shows/{}/episodes"
 
 epcode_re = re.compile(r"S(\d\d)E(\d\d)", re.IGNORECASE)
 
@@ -15,21 +16,18 @@ class ShowInfo:
     name = None
     summary = None
 
-def generate_name(season, episode):
-    return '{}.s{}e{}'.format(show_name, season, episode)
+class EpisodeInfo:
+    season = None
+    num = None
+    name = None
 
-def get_name_map(old_filenames):
-    name_map = {}
-    for filename in old_filenames:
-        match = epcode_re.search(filename)
-        if match:
-            name_map[filename] = generate_name(filename, *match.groups())
+def json_query(url):
+    with urllib.request.urlopen(url) as response:
+        return json.loads(response.readall().decode('utf-8'))
 
-
-def get_show(show_name):
+def get_show_info(show_name):
     query_url = show_search_url.format(urllib.parse.quote(show_name))
-    with urllib.request.urlopen(query_url) as response:
-        json_data = json.loads(response.readall().decode('utf-8'))
+    json_data = json_query(query_url)
     show_info = ShowInfo()
     show_info.id = json_data['id']
     show_info.name = json_data['name']
@@ -43,10 +41,35 @@ def confirm_show(show_info):
     print()
     return input("Correct? [Y/n] ").lower() in ('y', 'yes', '')
 
+def get_episodes_info(show_id):
+    query_url = episode_search_url.format(show_id)
+    json_data = json_query(query_url)
+    episodes_info = []
+    for episode_data in json_data:
+        episode_info = EpisodeInfo()
+        episode_info.season = episode_data['season']
+        episode_info.num = episode_data['number']
+        episode_info.name = episode_data['name']
+        episodes_info.append(episode_info)
+    return episodes_info
+
 def main():
     show_name = "Penn & Teller Bullshit!"
-    show_info = get_show(show_name)
+    show_info = get_show_info(show_name)
     print(confirm_show(show_info))
+
+    for ep in get_episodes_info(show_info.id):
+        print(ep.name)
+
+def generate_name(season, episode):
+    return '{}.s{}e{}'.format(show_name, season, episode)
+
+def get_name_map(old_filenames):
+    name_map = {}
+    for filename in old_filenames:
+        match = epcode_re.search(filename)
+        if match:
+            name_map[filename] = generate_name(filename, *match.groups())
 
 if __name__ == "__main__":
     main()
