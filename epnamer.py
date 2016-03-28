1b245ff735a9f4feca2cdcddd3900a9dcf07892c
Louis Warren <louis@lsw.nz>
2016-03-28 20:25:49 +1300

    Add api

--- epnamer.py
+++ epnamer.py
@@ -1,8 +1,12 @@
+import json
 import os
 import re
+import urllib.request
 
 target_dir = "~/epnamer/test"
-show_name = "Friends"
+show_name = "Penn & Teller Bullshit!"
+
+show_search_url = "http://api.tvmaze.com/singlesearch/shows?q={}"
 
 epcode_re = re.compile(r"S(\d\d)E(\d\d)", re.IGNORECASE)
 
@@ -18,8 +22,19 @@ def get_name_map(old_filenames):
 
 
 def main():
+    print("Data obtained via TVmaze <tvmaze.com>")
     dir_contents = os.listdir('test')
-    new_names = get_name_map(dir_contents)
+    #new_names = get_name_map(dir_contents)
+
+def get_show(show_name):
+    query_url = show_search_url.format(urllib.parse.quote(show_name))
+    with urllib.request.urlopen(query_url) as response:
+        json_data = json.loads(response.readall().decode('utf-8'))
+    return json_data['id'], json_data['name'], json_data['summary']
+
+
+def test():
+    print(get_show(show_name))
 
 if __name__ == "__main__":
-    main()
+    test()
