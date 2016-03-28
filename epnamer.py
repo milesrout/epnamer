4217a0f6e4154f0ab3e5009832ad6390e75dcf9c
Louis Warren <louis@lsw.nz>
2016-03-28 20:59:37 +1300

    cleanup

--- epnamer.py
+++ epnamer.py
@@ -3,12 +3,7 @@ import os
 import re
 import urllib.request
 
-target_dir = "~/epnamer/test"
 
-show_search_url = "http://api.tvmaze.com/singlesearch/shows?q={}"
-episode_search_url = "http://api.tvmaze.com/shows/{}/episodes"
-
-epcode_re = re.compile(r"S(\d\d)E(\d\d)", re.IGNORECASE)
 
 
 class ShowInfo:
@@ -26,6 +21,7 @@ def json_query(url):
         return json.loads(response.readall().decode('utf-8'))
 
 def get_show_info(show_name):
+    show_search_url = "http://api.tvmaze.com/singlesearch/shows?q={}"
     query_url = show_search_url.format(urllib.parse.quote(show_name))
     json_data = json_query(query_url)
     show_info = ShowInfo()
@@ -35,13 +31,13 @@ def get_show_info(show_name):
     return show_info
 
 def confirm_show(show_info):
-    print("Data obtained via TVmaze <tvmaze.com>")
     print("Selected '{}'".format(show_info.name))
     print("'''{}'''".format(show_info.summary))
     print()
     return input("Correct? [Y/n] ").lower() in ('y', 'yes', '')
 
 def get_episodes_info(show_id):
+    episode_search_url = "http://api.tvmaze.com/shows/{}/episodes"
     query_url = episode_search_url.format(show_id)
     json_data = json_query(query_url)
     episodes_info = []
@@ -54,6 +50,8 @@ def get_episodes_info(show_id):
     return episodes_info
 
 def main():
+    print("Data obtained via TVmaze <tvmaze.com>")
+
     show_name = "Penn & Teller Bullshit!"
     show_info = get_show_info(show_name)
     print(confirm_show(show_info))
@@ -61,10 +59,13 @@ def main():
     for ep in get_episodes_info(show_info.id):
         print(ep.name)
 
+    target_dir = "~/epnamer/test"
+
 def generate_name(season, episode):
     return '{}.s{}e{}'.format(show_name, season, episode)
 
 def get_name_map(old_filenames):
+    epcode_re = re.compile(r"S(\d\d)E(\d\d)", re.IGNORECASE)
     name_map = {}
     for filename in old_filenames:
         match = epcode_re.search(filename)
