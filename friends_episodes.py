0f1133308ab0d16e8bbe186fa88811ae7645e3ae
Louis Warren <louis@lsw.nz>
2016-05-03 21:20:11 +1200

    Fix hack

--- friends_episodes.py
+++ friends_episodes.py
@@ -4,6 +4,7 @@ from epnamer import *
 guide = tvmaze_guide('Friends')
 friends = list(guide)
 with open('friends_episodes.py', 'w') as f:
+    f.write('from epnamer import Episode\n')
     f.write('friends = [\n')
     for episode in friends:
         f.write(str(episode) + ',\n')
